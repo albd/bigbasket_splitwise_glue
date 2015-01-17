ab884b413f462b66fb77d87ab78268406ae4bc25
albert davies <alb.d91@gmail.com>
2015-01-18 05:28:53 +0530

    modified glue.py to write autokey phrase
    The code was modified to parse invoice and create autokey phrase for entering into splitwise bill. <tab> keystrokes were inserted between item and price on need basis.
    
    ==========known issues==============
    
    1. text entered skips some characters because of high speed of entering keystrokes.
    
    2. if the missed character is one of the tabs the whole allignment gets f**ked. This is a fatal error.

--- glue.py
+++ glue.py
@@ -2,8 +2,12 @@ import re
 from lxml import html
 
 file = open("samples/Invoice.html",'r')
+autokey = open("/home/albert/.config/autokey/data/My Phrases/bb.txt",'w')
 invoice = file.read()
 tree = html.fromstring(invoice)
 items = tree.xpath('//div[@class="delete-wpr"]/text()')
 items_clean = [re.match('^[\s~^]*(.*?)\s*$',elem).group(1) for elem in items]
 prices = tree.xpath('//td[@class="price" and @style="font-weight:bold;font-style:italic" or @style="font-weight:bold;"]/label/text()')
+prices_clean = [re.match('^\D*(.*)$',elem).group(1) for elem in prices]
+output = "<tab><tab>".join('%s<tab>%s' % t for t in zip(items_clean,prices_clean))
+autokey.write(output.encode('utf8'))
