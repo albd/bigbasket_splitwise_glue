ea3e174d4099b604a0062ece800e2b6be0a765ae
albert davies <alb.d91@gmail.com>
2015-01-18 05:35:54 +0530

    modified glue.py to make it an autokey script
    glue.py was first made into an autokey script with keyboard.send_keys function calls. This still retained the error of missed characters. Hence the workaround found was to fill the text in clipboard with clipboard.fill_clipboard followed by keyboard.send_keys("<ctrl>+v").
    
    to make clipboard.fill_clipboard work the following had to be done
    	Change this line in GtkClipboard's fill_clipboard():
    		self.clipBoard.set_text(contents.encode("utf-8"))
    	to this:
    		self.clipBoard.set_text(contents.encode("utf-8"), -1)
    
    this code seems to work without any bugs for the sample invoice.

--- glue.py
+++ glue.py
@@ -1,13 +1,20 @@
 import re
 from lxml import html
+import time
 
-file = open("samples/Invoice.html",'r')
-autokey = open("/home/albert/.config/autokey/data/My Phrases/bb.txt",'w')
+file = open("/mnt/data/technical/bigbasket_splitwise_glue/samples/Invoice.html",'r')
 invoice = file.read()
 tree = html.fromstring(invoice)
 items = tree.xpath('//div[@class="delete-wpr"]/text()')
 items_clean = [re.match('^[\s~^]*(.*?)\s*$',elem).group(1) for elem in items]
 prices = tree.xpath('//td[@class="price" and @style="font-weight:bold;font-style:italic" or @style="font-weight:bold;"]/label/text()')
 prices_clean = [re.match('^\D*(.*)$',elem).group(1) for elem in prices]
-output = "<tab><tab>".join('%s<tab>%s' % t for t in zip(items_clean,prices_clean))
-autokey.write(output.encode('utf8'))
+for i in zip(items_clean, prices_clean):
+    clipboard.fill_clipboard(i[0])
+    keyboard.send_keys("<ctrl>+v")
+    time.sleep(0.25)
+    keyboard.send_keys("<tab>")
+    clipboard.fill_clipboard(i[1])
+    keyboard.send_keys("<ctrl>+v")
+    time.sleep(0.25)
+    keyboard.send_keys("<tab><tab>")
