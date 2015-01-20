54125dbfd0ecbee1c94735f12292512ddfba53c5
ubuntu <ubuntu@ubuntu-VirtualBox>
2015-01-20 14:14:29 +0530

    closed loop changes to install.sh
    dependencies were added to install.sh. autokey configuration manager was
    launched prior to copying script to autokey directory. rectified patch
    command to ignore already patched file without prompting user.

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-sudo apt-get install autokey-gtk python-tk
-sudo patch /usr/lib/python2.7/dist-packages/autokey/scripting.py autokey.patch
-cp glue.py .glue.json '/home/albert/.config/autokey/data/Sample Scripts'
+sudo apt-add-repository ppa:cdekter/ppa
+sudo apt-get update
+sudo apt-get install autokey-gtk python-tk python-lxml
+sudo patch -N /usr/lib/python2.7/dist-packages/autokey/scripting.py autokey.patch
+autokey &
+cp glue.py .glue.json ~/.config/autokey/data/Sample\ Scripts
