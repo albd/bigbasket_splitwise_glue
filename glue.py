82ce399a256402ad8a0e33f1c74beaafe838dd87
albert davies <alb.d91@gmail.com>
2015-03-06 09:51:21 +0530

    commented glue.py to explain code
    The code has not been tested after commenting.

--- glue.py
+++ glue.py
@@ -1,20 +1,20 @@
-from Tkinter import Tk
+from Tkinter import Tk	#importing library for html file selection dialog
 from tkFileDialog import askopenfilename
-import re
-from lxml import html
-import time
+import re	#importing library for regular expressions
+from lxml import html	#importing library for lxml paths
+import time	#importing library for sleep
 
 Tk().withdraw()
-path = askopenfilename()
+path = askopenfilename()	#displays the file selection dialog to select invoice html file
 
-file = open(path,'r')
-invoice = file.read()
-tree = html.fromstring(invoice)
-items = tree.xpath('//div[@class="delete-wpr"]/text()')
-items_clean = [re.match('^[\s~^]*(.*?)\s*$',elem).group(1) for elem in items]
-prices = tree.xpath('//td[@class="price" and @style="font-weight:bold;font-style:italic" or @style="font-weight:bold;"]/label/text()')
-prices_clean = [re.match('^\D*(.*)$',elem).group(1) for elem in prices]
-for i in zip(items_clean, prices_clean):
+file = open(path,'r')	#open the stream into html invoice file
+invoice = file.read()	#read the content of invoice and stores in variable invoice
+tree = html.fromstring(invoice)	#converts the html string into a tree data structure of nested tags
+items = tree.xpath('//div[@class="delete-wpr"]/text()')	#xpath to extract content within tag for items. The result is a list of items with preceding and succeeding junk.
+items_clean = [re.match('^[\s~^]*(.*?)\s*$',elem).group(1) for elem in items]	#cleans the item list by stripping whitespaces, ^ and ~. The cleanup has been a prudent in that it removes only known junk.
+prices = tree.xpath('//td[@class="price" and @style="font-weight:bold;font-style:italic" or @style="font-weight:bold;"]/label/text()')	# xpath to extract content within tag for prices.
+prices_clean = [re.match('^\D*(.*)$',elem).group(1) for elem in prices]	#cleaned the content in prices.
+for i in zip(items_clean, prices_clean):	#while the keystrokes of items and prices could be simulated directly by using send_keys, it was found that the OS could not keep up with high rate of keystrokes. This led to some keystrokes being skipped. The workaround for this was to store the text in clipboard and then sending Ctrl+V keystrokes to paste the content. This would offload the job actually typing keystrokes to the kernel.
     clipboard.fill_clipboard(i[0])
     keyboard.send_keys("<ctrl>+v")
     time.sleep(0.1)
